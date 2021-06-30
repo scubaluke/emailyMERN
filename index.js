6e496a3cf087ef6da7c59824857cc84b58f32494
Luke Gartland <lukegartland@gmail.com>
2021-06-30 13:29:29 -0500

    added stripe payments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+// const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
@@ -23,8 +24,10 @@ app.use(
 )
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.json())
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 
