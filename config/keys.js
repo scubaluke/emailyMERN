18e65e73e6908f76e89f4db1c7a80de0fbc6e9e4
Luke Gartland <lukegartland@gmail.com>
2021-07-01 10:16:07 -0500

    .env test

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 
-// if (process.env.NODE_ENV === 'production') {
-//     // we are in production
-//     module.exports = require('./prod')
-// } else {
-//     // we are in development
-//   module.exports = require('./dev');
-// }
-module.exports = process.env
+if (process.env.NODE_ENV === 'production') {
+    // we are in production
+    module.exports = require('./prod')
+} else {
+    // we are in development
+  module.exports = require('./dev');
+}
 
