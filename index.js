e494821a41e88a7479f1c146d9610e51bee3ef3d
Luke Gartland <lukegartland@gmail.com>
2021-07-01 09:41:23 -0500

    added  heroku scripts

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-// const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
@@ -28,6 +27,16 @@ app.use(express.json())
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app)
+
+if(process.env.NODE_ENV === 'production') {
+    // express will server up  production assets like  main.js & main.css
+    app.use(express.static('client/build'))
+    // express will server up index.html if it doesn't recognize the route.
+   const path = require('path'); 
+   app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   })
+}
 
 const PORT = process.env.PORT || 5000
 
