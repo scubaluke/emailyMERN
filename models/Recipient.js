67f458e153abb7124dafa4b6bb1a6984bdb9c2db
Luke Gartland <lukegartland@gmail.com>
2021-07-02 10:26:15 -0500

    setting up sendgrid functions

--- models/Recipient.js
+++ models/Recipient.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const recipientSchema = new Schema({
-    email: string,
+    email: String,
     responded: { type: Boolean, default: false }
 })
 
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Survey = mongoose.model('surveys');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
@@ -15,5 +17,8 @@ module.exports = app => {
              _user: req.user.id,
              dateSent: Date.now(),
         })
+        // send the email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
     })
 }
